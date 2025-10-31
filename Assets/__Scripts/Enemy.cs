f9f0dc5ba8e8cbc31268aa3ca1a3a849cb45c134
taitt-e <123587963+taitt-e@users.noreply.github.com>
2025-10-31 01:06:17 -0500

    made projectiles destroy Enemies.

--- Assets/__Scripts/Enemy.cs
+++ Assets/__Scripts/Enemy.cs
@@ -45,4 +45,18 @@ public class Enemy : MonoBehaviour
         temPos.y -= speed * Time.deltaTime;
         pos = temPos;
     }
+
+    void OnCollisionEnter(Collision coll)
+    {
+        GameObject otherGO = coll.gameObject;
+        if (otherGO.GetComponent<ProjectileHero>() != null)
+        {
+            Destroy(otherGO); //Destroy the Projectile
+            Destroy(gameObject); // Destroy this Enemy GameObject
+        }
+        else
+        {
+            Debug.Log("Enemy hit by non-ProjectileHero" + otherGO.name); 
+        }
+    }
 }
