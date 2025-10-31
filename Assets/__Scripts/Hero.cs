7e622cea0dd3512130f52d809cb7d1ce2f12a512
taitt-e <123587963+taitt-e@users.noreply.github.com>
2025-10-31 01:01:36 -0500

    Gave _Hero ability to shoot.

--- Assets/__Scripts/Hero.cs
+++ Assets/__Scripts/Hero.cs
@@ -11,6 +11,8 @@ public class Hero : MonoBehaviour
     public float speed = 30;
     public float rollMult = -45;
     public float pitchMult = 30;
+    public GameObject projectilePrefab;
+    public float projectileSpeed = 40;
 
     [Header("Dynamic")]
     [Range(0, 4)]
@@ -45,6 +47,20 @@ public class Hero : MonoBehaviour
 
         //  Rotate the ship to make it feel more dynamic
         transform.rotation = Quaternion.Euler(vAxis * pitchMult, hAxis * rollMult, 0);
+
+        //  Allow the ship to fire
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            TempFire();
+        }
+    }
+
+    void TempFire()
+    {
+        GameObject projGO = Instantiate<GameObject>(projectilePrefab);
+        projGO.transform.position = transform.position;
+        Rigidbody rigidB = projGO.GetComponent<Rigidbody>();
+        rigidB.velocity = Vector3.up * projectileSpeed;
     }
 
     void OnTriggerEnter(Collider other)
