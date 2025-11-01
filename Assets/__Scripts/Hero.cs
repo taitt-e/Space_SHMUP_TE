cd21ffe2c2ffce5d27402f78a1bf4991bd7ffd45
taitt-e <123587963+taitt-e@users.noreply.github.com>
2025-11-01 01:44:05 -0500

    Made Delegate Event to fire

--- Assets/__Scripts/Hero.cs
+++ Assets/__Scripts/Hero.cs
@@ -20,10 +20,14 @@ public class Hero : MonoBehaviour
     private float _shieldLevel = 1; //Remember the underscore
     [Tooltip("This field holds a reference to the last triggering GameObject")]
     private GameObject lastTriggerGo = null;
+    // Declare a new delegate type WeaponFireDelegate
+    public delegate void WeaponFireDelegate();
+    // Create a WeaponFireDelegate event named fireEvent.
+    public event WeaponFireDelegate fireEvent;
 
     void Awake()
     {
-        if(S == null)
+        if (S == null)
         {
             S = this; // Set the Singleton only if it's null
         }
@@ -31,6 +35,7 @@ public class Hero : MonoBehaviour
         {
             Debug.LogError("Hero.Awake() - Attempted to assign second Hero.S!");
         }
+        fireEvent += TempFire;
     }
     // Update is called once per frame
     void Update()
@@ -49,9 +54,10 @@ public class Hero : MonoBehaviour
         transform.rotation = Quaternion.Euler(vAxis * pitchMult, hAxis * rollMult, 0);
 
         //  Allow the ship to fire
-        if (Input.GetKeyDown(KeyCode.Space))
+        //  Use the fireEvent to fire Weapons when the Spacebar is pressed.
+        if(Input.GetAxis("Jump") == 1 && fireEvent != null)
         {
-            TempFire();
+            fireEvent();
         }
     }
 
@@ -60,7 +66,11 @@ public class Hero : MonoBehaviour
         GameObject projGO = Instantiate<GameObject>(projectilePrefab);
         projGO.transform.position = transform.position;
         Rigidbody rigidB = projGO.GetComponent<Rigidbody>();
-        rigidB.velocity = Vector3.up * projectileSpeed;
+
+        ProjectileHero proj = projGO.GetComponent<ProjectileHero>();
+        proj.type = eWeaponType.blaster;
+        float tSpeed = Main.GET_WEAPON_DEFINITION(proj.type).velocity;
+        rigidB.velocity = Vector3.up * tSpeed;
     }
 
     void OnTriggerEnter(Collider other)
