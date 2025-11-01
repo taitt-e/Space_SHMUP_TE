4723251ff5cf1a8c7a17ad7ebedbd0d0ed770a38
taitt-e <123587963+taitt-e@users.noreply.github.com>
2025-11-01 02:42:24 -0500

    Make blink red when hit script for enemies

---
+++ Assets/__Scripts/BlindColorOnHit.cs
@@ -0,0 +1,61 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BlindColorOnHit : MonoBehaviour
+{
+    private static float blinkDuration = 0.1f; // # seconds to show damage
+    private static Color blinkColor = Color.red;
+
+    [Header("Dynamic")]
+    public bool showingColor = false;
+    public float blinkCompleteTime; // Time to stop showing the color
+
+    private Material[] materials; // Allthe Materials of this & its children
+    private Color[] originalColors;
+    private BoundsCheck bndCheck;
+
+    void Awake()
+    {
+        bndCheck = GetComponentInParent<BoundsCheck>();
+        // Get materials and colors for this GameObject and its children
+
+        materials = Utils.GetAllMaterials(gameObject);
+        originalColors = new Color[materials.Length];
+        for(int i = 0; i < materials.Length; i++)
+        {
+            originalColors[i] = materials[i].color;
+        }
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        if (showingColor && Time.time > blinkCompleteTime) RevertColors();
+    }
+
+    ///<summary>
+    /// Sets the Albedo color (i.e, the main color) of all materials in the materials array to blinkColor,
+    /// sets showingColor to true, and sets the time that the colors should be reverted.
+    /// </summary>
+    void SetColors()
+    {
+        foreach (Material m in materials)
+        {
+            m.color = blinkColor;
+            showingColor = true;
+            blinkCompleteTime = Time.time + blinkDuration;
+        }
+    }
+    ///<summary>
+    /// Reverts all materials in the materials array back to their original color
+    /// and sets showingColor to false
+    /// </summary>
+    void RevertColors()
+    {
+        for (int i = 0; i < materials.Length; i++)
+        {
+            materials[i].color = originalColors[i];
+        }
+        showingColor = false;
+    }
+}
