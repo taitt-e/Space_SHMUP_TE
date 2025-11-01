31e99dc976e30b0b5d18bff346ea6a30d48e8b66
taitt-e <123587963+taitt-e@users.noreply.github.com>
2025-11-01 02:25:52 -0500

    Updated OnCollisionEnter method

--- Assets/__Scripts/Enemy.cs
+++ Assets/__Scripts/Enemy.cs
@@ -47,17 +47,44 @@ public class Enemy : MonoBehaviour
         pos = temPos;
     }
 
+    //void OnCollisionEnter(Collision coll)
+    //{
+    //    GameObject otherGO = coll.gameObject;
+    //    if (otherGO.GetComponent<ProjectileHero>() != null)
+    //    {
+    //        Destroy(otherGO); //Destroy the Projectile
+    //        Destroy(gameObject); // Destroy this Enemy GameObject
+    //    }
+    //    else
+    //    {
+    //        Debug.Log("Enemy hit by non-ProjectileHero" + otherGO.name);
+    //    }
+    // }
+
     void OnCollisionEnter(Collision coll)
     {
         GameObject otherGO = coll.gameObject;
-        if (otherGO.GetComponent<ProjectileHero>() != null)
+        //Check for collisions with ProjectileHero
+        ProjectileHero p = otherGO.GetComponent<ProjectileHero>();
+        if (p != null)
         {
-            Destroy(otherGO); //Destroy the Projectile
-            Destroy(gameObject); // Destroy this Enemy GameObject
+            // Only damage this Enemy if it's on screen
+            if (bndCheck.isOnScreen)
+            {
+                // Get the damage amount from the Main WEAP_DICT.
+                health -= Main.GET_WEAPON_DEFINITION(p.type).damageOnHit;
+                if (health <= 0)
+                {
+                    // Destroy this Enemy
+                    Destroy(this.gameObject);
+                }
+            }
+            //Destroy the ProjectileHero regardless
+            Destroy(otherGO);
         }
         else
         {
-            Debug.Log("Enemy hit by non-ProjectileHero" + otherGO.name);
+            print("Enemy hit by non-ProjectileHero: " + otherGO.name);
         }
     }
 }
